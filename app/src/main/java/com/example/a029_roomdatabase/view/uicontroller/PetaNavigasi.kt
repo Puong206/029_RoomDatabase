267b914385be5a070f76f1f5fc7b067b7e7ba2d9
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-28 20:25:24 +0700

    Menambah composable SiswaApp di PetaNavigasi.kt

--- app/src/main/java/com/example/a029_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -3,15 +3,19 @@ package com.example.a029_roomdatabase.view.uicontroller
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.a029_roomdatabase.view.EntrySiswaScreen
 import com.example.a029_roomdatabase.view.HomeScreen
 import com.example.a029_roomdatabase.view.route.DestinasiEntry
 import com.example.a029_roomdatabase.view.route.DestinasiHome
 
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(navController: NavHostController, modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
