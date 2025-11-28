89407d5d1ead429e3463d5026254dcce664ab4f8
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-28 20:16:39 +0700

    Menambah implementasi DestinasiNavigasi pada DestinasiEntry

--- app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.a029_roomdatabase.view.route
 
-object DestinasiEntry {
+import com.example.a029_roomdatabase.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
