2e68c9d79f3668e49a0e4881e84c1dd10ca69eb9
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-28 19:07:55 +0700

    Menambah fungsi composable HomeScreen di HalamanHome.kt

--- app/src/main/java/com/example/a029_roomdatabase/view/HalamanHome.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/HalamanHome.kt
@@ -1,2 +1,19 @@
 package com.example.a029_roomdatabase.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a029_roomdatabase.viewmodel.HomeViewModel
+import com.example.a029_roomdatabase.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel { factory = PenyediaViewModel.Factory }
+){
+
+}
