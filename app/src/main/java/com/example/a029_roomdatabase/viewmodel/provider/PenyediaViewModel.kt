50bd072d839a702baab98da1f1b18aacb504aa0e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-28 18:42:35 +0700

    Menambah fungsi ekstensi aplikasiSiswa di PenyediaViewModel.kt

--- app/src/main/java/com/example/a029_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a029_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.a029_roomdatabase.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a029_roomdatabase.repositori.AplikasiSiswa
+import com.example.a029_roomdatabase.viewmodel.HomeViewModel
+
+
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
