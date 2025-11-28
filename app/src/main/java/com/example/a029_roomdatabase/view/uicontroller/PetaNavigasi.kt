976f3d8795fee4d472db59fb031b1c9e32c0cc95
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-28 20:22:35 +0700

    Menambah composable HostNavigasi untuk mengatur navigasi antara HomeScreen dan EntrySiswaScreen

--- app/src/main/java/com/example/a029_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,35 @@
 package com.example.a029_roomdatabase.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import com.example.a029_roomdatabase.view.EntrySiswaScreen
+import com.example.a029_roomdatabase.view.HomeScreen
+import com.example.a029_roomdatabase.view.route.DestinasiEntry
+import com.example.a029_roomdatabase.view.route.DestinasiHome
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
