124a908499c3ed52c6ee325434e4b3870dcf5af0
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 15:46:06 +0700

    Menambah navigasi ke DetailSiswaScreen di PetaNavigasi.kt

--- app/src/main/java/com/example/a029_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,12 +4,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.a029_roomdatabase.view.DetailSiswaScreen
 import com.example.a029_roomdatabase.view.EntrySiswaScreen
 import com.example.a029_roomdatabase.view.HomeScreen
+import com.example.a029_roomdatabase.view.route.DestinasiDetailSiswa
+import com.example.a029_roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.a029_roomdatabase.view.route.DestinasiEntry
 import com.example.a029_roomdatabase.view.route.DestinasiHome
+import com.example.a029_roomdatabase.viewmodel.DetailSiswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,6 +40,15 @@ fun HostNavigasi(
         }
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = { navController.navigateUp() })
         }
     }
 }
