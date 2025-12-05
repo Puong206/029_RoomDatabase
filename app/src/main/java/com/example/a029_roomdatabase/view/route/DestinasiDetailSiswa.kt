94a56dbfc37aafe2364b65e1967bdfabc16a742a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 15:23:50 +0700

    Menambah konstanta itemIdArg di object DestinasiDetailSiswa

--- app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -5,5 +5,6 @@ import com.example.a029_roomdatabase.R
 object DestinasiDetailSiswa : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
     
 }
