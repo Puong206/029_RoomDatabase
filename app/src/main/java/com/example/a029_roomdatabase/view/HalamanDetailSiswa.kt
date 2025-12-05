ef9f1bea673c37589670e965d980a2cb394c1dba
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 15:50:37 +0700

    Menambah implementasi AlertDialog konfirmasi hapus di HalamanDetailSiswa.kt

--- app/src/main/java/com/example/a029_roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/HalamanDetailSiswa.kt
@@ -1,9 +1,14 @@
 package com.example.a029_roomdatabase.view
 
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a029_roomdatabase.R
 import com.example.a029_roomdatabase.viewmodel.DetailViewModel
 import com.example.a029_roomdatabase.viewmodel.provider.PenyediaViewModel
 
@@ -24,5 +29,19 @@ private fun DeleteConfirmationDialog(
     onDeleteCancel: () -> Unit,
     modifier: Modifier = Modifier
 ){
-
+    AlertDialog(onDismissRequest = {/*Do nothing */},
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.tanya)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = stringResource(R.string.yes))
+            }
+        }
+    )
 }
