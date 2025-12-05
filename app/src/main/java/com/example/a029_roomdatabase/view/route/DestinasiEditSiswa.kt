08696164ccc5da7ea391402037696a1ea9b7e050
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 20:42:39 +0700

    Mengimplementasikan DestinasiNavigasi pada DestinasiEditSiswa dan menambahkan route with argument

--- app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiEditSiswa.kt
@@ -1,4 +1,10 @@
 package com.example.a029_roomdatabase.view.route
 
-object DestinasiEditSiswa {
+import com.example.a029_roomdatabase.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "edit_siswa"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
