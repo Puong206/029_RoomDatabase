cd6228c838fe341810ca4d0c212993336cf8ac39
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 15:22:42 +0700

    Mengimplementasikan interface DestinasiNavigasi pada object DestinasiDetailSiswa

--- app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -1,4 +1,8 @@
 package com.example.a029_roomdatabase.view.route
 
-object DestinasiDetailSiswa {
+import com.example.a029_roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    
 }
