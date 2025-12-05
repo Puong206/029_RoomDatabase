22462ca7fecd21224c018b70f84c6eefe6b27a55
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 15:17:39 +0700

    Menambah implementasi awal DetailViewModel dengan idSiswa dan repositori

--- app/src/main/java/com/example/a029_roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/a029_roomdatabase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.a029_roomdatabase.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a029_roomdatabase.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+    
+}
