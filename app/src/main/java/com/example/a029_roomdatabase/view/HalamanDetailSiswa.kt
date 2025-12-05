f82ab321a883fe94ee0b1090acc0ab4ac4e4d940
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-05 16:23:03 +0700

    Menambahkan Scaffold, TopAppBar, FAB, dan logika delete pada HalamanDetailSiswa

--- app/src/main/java/com/example/a029_roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/a029_roomdatabase/view/HalamanDetailSiswa.kt
@@ -8,17 +8,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -28,8 +37,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a029_roomdatabase.R
 import com.example.a029_roomdatabase.room.Siswa
+import com.example.a029_roomdatabase.view.route.DestinasiDetailSiswa
 import com.example.a029_roomdatabase.viewmodel.DetailViewModel
 import com.example.a029_roomdatabase.viewmodel.provider.PenyediaViewModel
+import com.example.a029_roomdatabase.viewmodel.toSiswa
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,11 +51,49 @@ fun DetailSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetailSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.uiDetailState.collectAsState()
+            FloatingActionButton(
+                onClick = {
+                    //navigateToEditItem(uiState.value.detailSiswa.id)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        val uiState = viewModel.uiDetailState.collectAsState()
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            detailSiswaUiState = uiState.value,
+            onDelete = { coroutineScope.launch {
+                viewModel.deleteSiswa()
+                navigateBack()
+            }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
 }
 
 @Composable
 private fun BodyDetailDataSiswa(
-    detailSiswaUiState: DetailSiswaUiState,
+    detailSiswaUiState: DetailViewModel.DetailSiswaUiState,
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -94,7 +144,7 @@ fun DetailDataSiswa(
             verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
         ) {
             BarisDetailData(
-                labelResID = R.string.nama1,
+                labelResId = R.string.nama1,
                 itemDetail = siswa.nama,
                 modifier = Modifier.padding(
                     horizontal = dimensionResource(
@@ -104,7 +154,7 @@ fun DetailDataSiswa(
                 )
             )
             BarisDetailData(
-                labelResID = R.string.alamat1,
+                labelResId = R.string.alamat1,
                 itemDetail = siswa.alamat,
                 modifier = Modifier.padding(
                     horizontal = dimensionResource(
@@ -114,7 +164,7 @@ fun DetailDataSiswa(
                 )
             )
             BarisDetailData(
-                labelResID = R.string.telpon1,
+                labelResId = R.string.telpon1,
                 itemDetail = siswa.telpon,
                 modifier = Modifier.padding(
                     horizontal = dimensionResource(
@@ -127,7 +177,6 @@ fun DetailDataSiswa(
 
     }
 }
-
 
 @Composable
 private fun BarisDetailData(
